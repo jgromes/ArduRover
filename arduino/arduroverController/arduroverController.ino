403f9411be6e8d59100b3e7b56c7d8e6d3e3b741
Jan Gromeš <jgromes@users.noreply.github.com>
2017-12-29 10:46:55 +0100

    Update arduroverController.ino

--- arduino/arduroverController/arduroverController.ino
+++ arduino/arduroverController/arduroverController.ino
@@ -1,50 +1,65 @@
+// Include LoRa library
 #include <LoRaLib.h>
 
-uint8_t cmd[4] = {0, 0, 0, 0};
-
+// Create instance of the LoRa object
 LoRa lora;
 
+// Packet to store data that will be transmitted by LoRa (commands)
+uint8_t cmd[4] = {0, 0, 0, 0};
 Packet out("01:23:45:67:89:AB:CD:EF", cmd, 4);
-Packet in;
+
+// Packet to store data that will be received by LoRa (responses)
+uint8_t cmdIn[4] = {0, 0, 0, 0};
+Packet in("01:23:45:67:89:AB:CD:EF", cmdIn, 4);
 
 void setup() {
+  // Begin communication via USB
   Serial.begin(9600);
+  // Begin communication via LoRa
   lora.begin();
 }
 
+// This flag will be set when a packet was transmitted sucessfully 
 bool sent = true;
 void loop() {
+  // Check if a new command was received
   if(Serial.available() > 0) {
+    // Reset the flag
     sent = false;
+    // Read all command bytes and store them to output buffer
     while(Serial.available()) {
       Serial.readBytes(cmd, 4);
     }
+    // Copy command data to the output packet payload
     for(int i = 0; i < 4; i++) {
       out.data[i] = cmd[i];
-      Serial.println(cmd[i]);
     }
   }
-  
+
+  // Variable to store LoRa communication state
   uint8_t state;
+
+  // If the current packet wasn't sent yet, send it
   if(!sent) {
     state = lora.transmit(out);
   }
-  
+
+  // If the packet was sent successfully, set the sent flag
   if((state == ERR_NONE) && (!sent)) {
     sent = true;
-    for(int i = 0; i < 4; i++) {
-      Serial.write(out.data[i]);
-    }
-    Serial.println();
   }
 
+  // Try to receive a packet
   state = lora.receive(in);
   if(state == ERR_NONE) {
+    // If the reception was successfull, write all the response data to the USB port
     for(int i = 0; i < 4; i++) {
       Serial.write(in.data[i]);
     }
-  } else if(state == ERR_CRC_MISMATCH) {
-    
+
+    // If the response is 0xA0, the last packet arrived malformed and has to be resend
+    if(cmdIn[0] == 0xA0) {
+      lora.transmit(out);
+    }
   }
 }
-
